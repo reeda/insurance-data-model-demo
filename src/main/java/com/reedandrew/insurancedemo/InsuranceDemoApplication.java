8909e81a6f60cf0d21af8ec03d40a6d2edf465af
areed <Andrew.Reed@ipsoft.com>
2017-04-08 18:08:29 -0400

    initial project generated from start.spring.io

---
+++ src/main/java/com/reedandrew/insurancedemo/InsuranceDemoApplication.java
@@ -0,0 +1,12 @@
+package com.reedandrew.insurancedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceDemoApplication.class, args);
+	}
+}
